7a730d0d2a3f9aaea0e9871b860d08f2fa0f4a7e
Dilmaya Hamal <dilmayahamal09@gmail.com>
2024-09-19 19:53:22 +0545

    quiz app

---
+++ src/data.js
@@ -0,0 +1,331 @@
+export const quizData = {
+  quizzes: [
+
+// =====================Computer========================
+
+    {
+      title: "Computer",
+      icon: "/images/Blue_computer_icon.svg",
+      questions: [
+        {
+          question: "Who is known as the father of computer?",
+          options: [
+            "Ada Lovelace",
+            "Alan Turing",
+            "Charles Babbage",
+            "Blaise Pascal",
+          ],
+          answer: "Charles Babbage",
+        },
+        {
+          question:
+            "Which is the brain of the computer?",
+          options: [
+            "ALU","CPU","Memory","Mouse",
+          ],
+          answer: "CPU",
+        },
+        {
+          question:
+            "What was the first internet search engine?",
+          options: ["Yahoo", "Google", "Archie", "Lycos"],
+          answer: "Archie",
+        },
+        {
+          question: "Who was the inventor of the world wide web?",
+          options: [
+            "Larry Page",
+            "Jerry Yang",
+            "Vint Cerf",
+            "Tim Berners-Lee",
+          ],
+          answer:
+            "Tim Berners-Lee",
+        },
+        {
+          question: "What is the name of the first computer virus?",
+          options: ["Creeper", "Trojan", "Rabbit", "Mydoom"],
+          answer: "Creeper",
+        },
+        {
+          question: "Which of the below is not an extension of image file?",
+          options: [".jpg", ".doc", ".png", ".gif"],
+          answer: ".doc",
+        },
+        {
+          question:
+            "What is the main web page of a website?",
+          options: ["Navigator page", "Dashboard page", "Book mark page", "Home page"],
+          answer: "Home page",
+        },
+        {
+          question: "What is an error in a computer program called?",
+          options: ["Mistake", "Spam", "Bug", "Err"],
+          answer: "Bug",
+        },
+        {
+          question: "Which device is required for the internet connection",
+          options: [
+            "Modern",
+            "Router",
+            "LAN cable",
+            "Pen drive",
+          ],
+          answer: "Modern",
+        },
+        {
+          question: "What was the name of first computer network?",
+          options: [
+            "MAC",
+            "SNoBoL",
+            "ARPANET",
+            "None",
+          ],
+          answer: "ARPANET",
+        },
+      ],
+    },
+
+// =====================Science========================
+
+    {
+      title: "Science",
+      icon: "/images/science.svg",
+      questions: [
+        {
+          question: "Who is father of secience?",
+          options: [
+            "Galileo Galilei",
+            "Sir Isaac Newton",
+            "Albert Einstein",
+            "Nikola Tesla",
+          ],
+          answer: "Albert Einstein",
+        },
+        {
+          question: "What is the SI unit of temperature?",
+          options: [
+            "Celcius",
+            "Kelvin",
+            "Farenheit",
+            "Thermometer",
+          ],
+          answer: "Kelvin",
+        },
+        {
+          question:
+            "Which acid present in apple?",
+          options: ["Acetic acid", "Formic acid", "Oxalic acid", "Malic acid"],
+          answer: "Malic acid",
+        },
+        {
+          question: "Which is the biggest planet of solar system?",
+          options: ["Saturn", "Mercury", "Jupiter", "Earth"],
+          answer: "Jupiter",
+        },
+        {
+          question: "Which is the smallest planet of solar system?",
+          options: ["Neptune", "Mercury", "Uranus", "Jupiter"],
+          answer: "Mercury",
+        },
+        {
+          question:
+            "What is the highest mountain in the solor system?",
+          options: [
+            "Mauna Kea",
+            "Mount Everest",
+            "Mons Mouton",
+            "Olympus Mons",
+          ],
+          answer: "Olympus Mons",
+        },
+        {
+          question:
+            "Which gas is commonly known as laughing gas?",
+          options: ["Nitrogen", "Oxygen", "Nitrous Oxide", "Carbon Dioxide"],
+          answer: "Nitrous Oxide",
+        },
+        {
+          question: "What is the chemical symbol for tin?",
+          options: ["Ti", "Tn", "Sn", "Sb"],
+          answer: "Sn",
+        },
+        {
+          question: "What is percent of human body is water?",
+          options: [
+            "40%", "50%", "60%", "70%",
+          ],
+          answer: "60%",
+        },
+        {
+          question: "What is the time taken by the sunlight to reach earth's surface?",
+          options: [
+            "5min & 50sec", "6min & 10sec", "8min & 20sec", "9min & 30sec",
+          ],
+          answer: "8min & 20sec",
+        },
+      ],
+    },
+
+// =====================World========================
+
+    {
+      title: "World",
+      icon: "/images/earth-icon.svg",
+      questions: [
+        {
+          question:
+            "Which is the world's longest river?",
+          options: [
+            "Nile","Amazon","Yangtze","Mississippi"
+            
+          ],
+          answer: "Nile",
+        },
+        {
+          question: "Which city is known as the city of lights?",
+          options: [
+           "Paris","Beijing","Tokyo","Sydney"
+          ],
+          answer: "Paris",
+        },
+        {
+          question: "Which is the most populated country in the world?",
+          options: [
+           "China","United States", "India","Indonesia"
+          ],
+          answer: "India",
+        },
+        {
+          question:
+          "What is the height of Mount Everest in meters?",
+          options: [
+            "8611.45","8848.86","8948.66","9948.76"
+          ],
+          answer: "8,848.86 ",
+        },
+        {
+          question: "Which one of the following is not a land locked country?",
+          options: [
+           "Mongolia","Nepal","China","Afghanistan"
+          ],
+          answer: "China",
+        },
+        {
+          question: "Which is the widest waterfall in the world?",
+          options: [
+           "Khone","Niagara","Mocona","Victoria",
+          ],
+          answer: "Khone",
+        },
+        {
+          question: "What is the modern name of Persia?",
+          options: [
+           "Iraq","Syria","Turkey","Iran",
+          ],
+          answer: "Iran",
+        },
+        {
+          question: "Which contient has the highest number of countries?",
+          options: [
+           "Asia","Africa","North America","Europe",
+          ],
+          answer: "Africa",
+        },
+        {
+          question: "The first country to print a book in the world?",
+          options: [
+            "China","Japan","Australia","Bhutan"
+          ],
+          answer: "China",
+        },
+        {
+          question: "Which is the first country in the world to launch an artificial satellite into space?",
+          options: [
+           "China","Russia","Italy","Japan",
+          ],
+          answer: "Russia",
+        },
+      ],
+    },
+
+// =====================Nepal========================
+
+    {
+      title: "Nepal",
+      icon: "/images/flagofNp.svg",
+      questions: [
+        {
+          question: "Where is the Gateway of Nepal located?",
+          options: [
+            "Janakpur","Birgunj","Biratnagar","Nepalgunj"
+          ],
+          answer: "Birgunj",
+        },
+          {
+            question: "Which is the biggest district of Nepal?",
+            options: [
+              "Dolpa","Rolpa","Surkhet","Mugu"
+            ],
+            answer: "Dolpa",
+          },
+          {
+            question: "Which is national flower of Nepal?",
+            options: [
+              "Lily","Lotus","Rose","Rhododendron"
+            ],
+            answer: "Rhododendron",
+          },
+          {
+            question: "What is the largest city of Nepal?",
+            options: [
+              "Kathmandu","Pokhara","Janakpur","Lumbini"
+            ],
+            answer: "Kathmandu",
+          },
+          {
+            question: "What is the name of first satellite launched in Nepal?",
+            options: [
+              "HimaliSat-1","NepSat-1","SagarmathaSat-1","GaganSat-1"
+            ],
+            answer: "NepSat-1",
+          },
+          {
+            question: "What is the national sport of Nepal?",
+            options: [
+              "Kabaddi","Cricket","Volleyball","Football"
+            ],
+            answer: "Volleyball",
+          },
+          {
+            question: "Who was the first king of Nepal?",
+            options: [
+              "Bhuktaman","Prithvi Narayan Shah","Mandeev","Anshu Verma",
+            ],
+            answer: "Prithvi Narayan Shah",
+          },
+          {
+            question: "Which is the first collage of Nepal?",
+            options: [
+              "Tribhuvan University","Tri-candra Collage","Durbar High School","IB School",
+            ],
+            answer: "Tri-chandra Collage",
+          },
+          {
+            question: "Who is the person who used the world jai Nepal for the first time?",
+            options: [
+              "Mahendra Shah","Shukraj Shastri","Jang Bahadur Rana","Prithvi Narayan Shah"
+            ],
+            answer: "Shukraj Shatri",
+          },
+          {
+            question: "Which bank in Nepal is known as the Central Bank?",
+            options: [
+              "Rastriya Banijya Bank","Nepal Rastra Bank","Nepal Investment Bank","Agricultural Development Bank",
+            ],
+            answer: "Nepal Rastra Bank",
+          },
+      ],
+    },
+  ],
+};
